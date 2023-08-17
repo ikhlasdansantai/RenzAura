2a782ffd740c4307ed93c0f438ab586a9c1945db
ikhlasdansantai <initesting5@gmail.com>
2023-08-17 22:31:09 +0700

    menambahkan konten tambahan untuk comp home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>This Is America</div>;
+  return (
+    <>
+      <h2 className="text-lg dark:text-white">HI, Selamat Datang! 🎉</h2>
+      <p className="text-secondaryText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci harum iusto quis vel aliquam? Quae cum quibusdam eius cupiditate vitae.</p>
+      <div id="contents">
+        
+      </div>
+    </>
+  );
 }
