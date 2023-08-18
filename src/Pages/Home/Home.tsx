8cc10300ee79c512e54ab2cd0d15da9e18e51c51
ikhlasdansantai <initesting5@gmail.com>
2023-08-18 22:43:37 +0700

    menambahkan konten pada home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,38 @@
+import "./home.css";
+import { Icon } from "@iconify/react";
+
 export default function Home() {
   return (
-    <>
-      <h2 className="text-lg dark:text-white">HI, Selamat Datang! 🎉</h2>
-      <p className="text-secondaryText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci harum iusto quis vel aliquam? Quae cum quibusdam eius cupiditate vitae.</p>
-      <div id="contents">
-        
+    <div id="home">
+      <div className="titles relative">
+        <h1 className="text-2xl dark:text-white">Hi, Selamat Datang! 🎉</h1>
+        <p className="dark:text-[#a3a3a3] mt-2">Memperkenalkan sebuah platform untuk kamu yang mau belanja dengan mudah dengan harga yang murah meriah, selamat berbelanja :D.</p>
       </div>
-    </>
+      <div id="contents" className="mt-10">
+        <div className="container flex justify-between items-center">
+          <div className="info flex items-center gap-2">
+            <Icon icon="cib:instagram" className="icon" />
+            <span>Instagram</span>
+          </div>
+          <span>Lihat Selengkapnya </span>
+        </div>
+        <div className="container-cards">
+          <div className="card">
+            <figure>
+              <img src="" alt="" />
+            </figure>
+            <div className="title">
+              <figure className="profile-car">
+                <img src="" alt="" />
+              </figure>
+              <div className="name-bio">
+                <h2>RenzAura</h2>
+                <p>12 Hours Ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
