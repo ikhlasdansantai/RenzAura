b67312625166e57244c6a1611c3c6b041a5241a6
ikhlasdansantai <initesting5@gmail.com>
2023-08-19 20:55:50 +0700

    menambahkan class darkmode pada text sosial media kami

--- src/Components/sidebarMenus/SocialMedia.tsx
+++ src/Components/sidebarMenus/SocialMedia.tsx
@@ -6,8 +6,8 @@ interface SocialMediaProps {
 export default function SocialMedia({ icons }: SocialMediaProps): JSX.Element {
   return (
     <div className="mt-8">
-      <span className="dark:text-secondaryText">Sosial Media Kami</span>
-      <div className="socialMedia flex gap-2 items-center px-3 py-4">
+      <span className="dark:text-[#a3a3a3]">Sosial Media Kami</span>
+      <div className="socialMedia flex gap-4 items-center px-3 py-4">
         {icons.map((sosmed, index) => {
           return <Icon icon={sosmed} key={index} className="text-lg icon" />;
         })}
