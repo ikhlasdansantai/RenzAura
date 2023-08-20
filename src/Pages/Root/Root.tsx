51f9cba313acddec31993abccd32159e5cc7b63c
ikhlasdansantai <initesting5@gmail.com>
2023-08-20 20:22:31 +0700

    memindahkan fungsionalitas navmobile menu ke root comp

--- src/Pages/Root/Root.tsx
+++ src/Pages/Root/Root.tsx
@@ -1,13 +1,20 @@
 import CTAButton from "../../Components/SideBar/SideBar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
-export default function Home() {
+export default function Root() {
+  const [navMobileActive, setNavMobileActive] = useState(false);
+
+  const handleNavMobile = () => {
+    setNavMobileActive((navMobileActive) => !navMobileActive);
+  };
+
   return (
-    <div>
-      <CTAButton />
-      <div className="p-4 sm:ml-64">
+    <>
+      <CTAButton handleNavMobile={handleNavMobile} navMobileActive={navMobileActive} />
+      <main id="main" className={`p-4 sm:ml-64 ${navMobileActive ? "hidden" : ""}`}>
         <Outlet />
-      </div>
-    </div>
+      </main>
+    </>
   );
 }
