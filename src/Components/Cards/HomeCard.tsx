40b74f1fc4d37bfdce6f67d0ae5b489b866cdd8e
ikhlasdansantai <initesting5@gamil.com>
2023-08-22 21:32:13 +0700

    FIX BUG ( memperbaiki perilaku navLink ketika di klik di mobile )

--- src/Components/Cards/HomeCard.tsx
+++ src/Components/Cards/HomeCard.tsx
@@ -25,9 +25,9 @@ export default function HomeCard({ imgProfile, imgPost, author, date, link }: Ho
   };
 
   return (
-    <div onClick={() => handleLinkTo()} className="home-card w-[20rem] cursor-pointer">
+    <div onClick={() => handleLinkTo()} className="home-card snap-end flex-shrink-0 w-[22rem] md:w-[20rem] cursor-pointer pr-4">
       <figure className="relative">
-        <img src={imgPost} alt="instagramPostPict" height="100%" width="100%" className="block max-w-full " />
+        <img src={imgPost} alt="instagramPostPict" height="100%" width="100%" className="block max-w-full" />
         <SosmedBeta />
       </figure>
       <div className="titles flex items-center gap-2 mt-2">
