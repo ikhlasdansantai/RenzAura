ce203883f3948358c8be49adf60f75a719b8e604
ikhlasdansantai <initesting5@gmail.com>
2023-08-26 21:24:02 +0700

    minor( mengganti text button untuk form )

--- src/Pages/Form/Form.tsx
+++ src/Pages/Form/Form.tsx
@@ -34,7 +34,7 @@ export default function Form() {
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Sign in
+                Kirim
               </button>
               <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">Terimakasih atas kerjasamanya :)</p>
             </form>
