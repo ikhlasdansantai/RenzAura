a4d118fb413726e993e508328874226ed1c1fb6f
ikhlasdansantai <initesting5@gmail.com>
2023-08-26 21:23:33 +0700

    UI( memperbaiki grid fashion dan menonaktifkan tampilan yang tidak diperlukan )

--- src/Pages/Fashion/Fashion.tsx
+++ src/Pages/Fashion/Fashion.tsx
@@ -19,14 +19,14 @@ export default function Fashion({ items }: any): JSX.Element {
         </p>
       </div>
       <div id="contents" className="mt-10">
-        <div className="container flex justify-between items-center">
+        {/* <div className="container flex justify-between items-center">
           <div className="info flex items-center gap-2">
             <Icon icon="cib:instagram" className="icon" />
             <span className="dark:text-secondText">Instagram</span>
           </div>
           <span className="dark:text-secondText">Lihat Selengkapnya</span>
-        </div>
-        <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-3 md:flex-row gap-10">
+        </div> */}
+        <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-3 min-[1400px]:grid-cols-4  md:flex-row gap-10">
           {items.map((item: any) => (
             <Link to={`/pakaian/${item.id}`} key={item.id}>
               <div className="p-3 rounded-md bg-white border shadow-lg">
