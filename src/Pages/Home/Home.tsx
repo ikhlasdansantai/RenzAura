0f26960f0f4f65c106165e629a1e87dd479e7e3d
ikhlasdansantai <initesting5@gmail.com>
2023-08-27 10:52:09 +0700

    feat( menambahkan Relative Time Formatting seperti ig dan yt )

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import igPost1 from "../../assets/instagram_post/1.webp";
 import igPost2 from "../../assets/instagram_post/2.webp";
+import igPost3 from "../../assets/instagram_post/3.webp";
 import userImgProfile from "../../assets/profile_users/368172058_200014439727799_4548607432220709411_n.jpg";
 import "../../index.css";
 import HomeCard from "../../Components/Cards/HomeCard";
@@ -20,24 +21,46 @@ export default function Home() {
       imgPost: igPost1,
       imgProfile: userImgProfile,
       author: "Renz Aura",
-      date: "2 Hours Ago",
+      date: "19 August 2023",
       link: "https://www.instagram.com/renz_aura/",
     },
     {
       imgPost: igPost2,
       imgProfile: userImgProfile,
       author: "Renz Aura",
-      date: "5 Hours Ago",
+      date: "22 August 2023",
       link: "https://www.instagram.com/renz_aura/",
     },
     {
-      imgPost: igPost1,
+      imgPost: igPost3,
       imgProfile: userImgProfile,
       author: "Renz Aura",
-      date: "10 Hours Ago",
+      date: "26 August 2023",
       link: "https://www.instagram.com/renz_aura/",
     },
   ];
+
+  function convertToTimestamp(dateString: string): number {
+    const timestamp = Date.parse(dateString);
+    return timestamp;
+  }
+  function getTimeAgo(timestamp: any) {
+    const currentDate = new Date();
+    const targetDate = new Date(timestamp);
+
+    const timeDifference = currentDate.getTime() - targetDate.getTime();
+    const minutesDifference = Math.floor(timeDifference / (1000 * 60)); // Menghitung selisih dalam menit
+
+    if (minutesDifference < 60) {
+      return `${minutesDifference} Minutes Ago`;
+    } else if (minutesDifference < 1440) {
+      const hoursDifference = Math.floor(minutesDifference / 60);
+      return `${hoursDifference} Hours Ago`;
+    } else {
+      const daysDifference = Math.floor(minutesDifference / 1440);
+      return `${daysDifference} Days Ago`;
+    }
+  }
 
   return (
     <div id="home">
@@ -55,7 +78,9 @@ export default function Home() {
         </div>
         <div className="container-cards flex flex-row w-94 mt-6 gap-4 snap-x snap-proximity overflow-x-scroll overflow-y-hidden pb-3 xl:snap-none xl:overflow-hidden">
           {homeContentDatas.map((data, index) => {
-            return <HomeCard key={index} imgProfile={data.imgProfile} imgPost={data.imgPost} author={data.author} date={data.date} link={data.link} />;
+            const timestamp = convertToTimestamp(data.date);
+            const timeAgo = getTimeAgo(timestamp);
+            return <HomeCard key={index} imgProfile={data.imgProfile} imgPost={data.imgPost} author={data.author} date={timeAgo} link={data.link} />;
           })}
         </div>
       </div>
