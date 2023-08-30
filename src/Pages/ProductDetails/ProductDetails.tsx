18b9a8c760404141cad8cc437ef03ce5472ae9fe
ikhlasdansantai <initesting5@gmail.com>
2023-08-30 11:59:05 +0700

    feat( menambahkan fungsi button dan validasi product jika product tidak ada )

--- src/Pages/ProductDetails/ProductDetails.tsx
+++ src/Pages/ProductDetails/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import dana from "../../assets/Icons/DANA.svg";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import Swal from "sweetalert2";
 
 // interface ProductDetailsProps {
 //   imgPost: string;
@@ -16,7 +17,32 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function ProductDetails({ product }: any) {
   const { id } = useParams();
-  const getProduct = product.find((item: any) => item.id === Number(id));
+  let getProduct = product.find((item: any) => item.id === Number(id));
+  const { title, desc, price, size, imgPost, productLink } = getProduct;
+
+  const handleClick = (productLink: string, e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if (productLink === "empty") {
+      Swal.fire({
+        title: "Produk Belum Tersedia :(",
+        text: "Produk akan segera dirilis di Shoope!, Tetaplah bersama kami dan nantikan kehadirannya dengan penuh antusiasme :D",
+        icon: "error",
+      });
+    } else {
+      Swal.fire({
+        title: "Sedang dialihkan ke Shoope",
+        text: "Harap tunggu sejenak",
+        icon: "info",
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        timer: 2000,
+      }).then(() => {
+        window.location.href = productLink;
+      });
+    }
+  };
 
   const PaymentAvailable = () => {
     return (
@@ -36,19 +62,21 @@ export default function ProductDetails({ product }: any) {
       </Link>
       <div className="flex flex-col mt-10 sm:mt-20 justify-between items-center min-[950px]:flex-row min-[1200px]:justify-evenly min-[1400px]:w-[70rem] min-[1400px]:justify-center min-[1400px]:mx-auto min-[1400px]:gap-20">
         <figure className="min-[950px]:w-[20rem] min-[1120px]:w-[25rem] min-[1400px]:w-[35rem]">
-          <img src={getProduct.imgPost} alt="productIMG" height="100%" width="100%" />
+          <img src={imgPost} alt="productIMG" height="100%" width="100%" />
         </figure>
         <div className="min-[950px]:w-1/2 mt-8">
-          <h2 className="text-3xl dark:text-white">{getProduct.title}</h2>
-          <p className="mt-4 text-gray-500">{getProduct.desc}</p>
+          <h2 className="text-3xl dark:text-white">{title}</h2>
+          <p className="mt-4 text-gray-500">{desc}</p>
           <div className="mt-4 flex gap-3 text-gray-500">
             <p>Ukuran:</p>
-            <span>{getProduct.size}</span>
+            <span>{size}</span>
           </div>
-          <p className="mt-4 text-gray-900 dark:text-gray-500">
-            Subtotal: <span className="font-semibold text-[#EAB308]">Rp.{getProduct.price.toLocaleString("id-ID")}</span>
+
+          <p className={`${productLink === "empty" ? "line-through" : ""} mt-4 text-gray-900 dark:text-gray-500 `}>
+            Subtotal: <span className="font-semibold text-[#EAB308]">{`${productLink === "empty" ? "Rahasia" : `Rp.${price.toLocaleString("id-ID")}`}`}</span>
           </p>
           <button
+            onClick={(e) => handleClick(productLink, e)}
             type="submit"
             className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-6 min-[1400px]:w-[20rem]"
           >
