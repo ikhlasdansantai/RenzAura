a8b325ccd9732e31af53cede9e49af4b0a2c503e
ikhlasdansantai <initesting5@gmail.com>
2023-10-03 13:30:02 +0700

    fix back to home btn link on NFP

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -18,7 +18,7 @@ export default function NotFoundPage() {
         <p className="text-center ">Terjadi kesalahan, karena kamu mengakses halaman yang tidak ditemukan</p>
         <span className="text-xs block mt-1">pesan ERROR: {errorMessage}</span>
         <Link
-          to={"/RenzAura/"}
+          to={"/"}
           type="button"
           className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
